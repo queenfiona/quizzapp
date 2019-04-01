4f069e6473e2cb60a637602693886b5dbfcaaad5
queenfiona <fionaitya@gmail.com>
2019-04-02 00:20:55 +0300

    save high scores in local storage

--- js/end.js
+++ js/end.js
@@ -3,6 +3,8 @@ const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 // most recent score is obtained from game.js
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+const highScores =  JSON.parse(localStorage.getItem("highScores")) || [];
+console.log(highScores);
 
 finalScore.innerText = mostRecentScore;
 username.addEventListener("keyup",()=>{
@@ -11,4 +13,19 @@ username.addEventListener("keyup",()=>{
 saveHighScore = (e)=>{
 	console.log("clicked the save btn");
 	e.preventDefault();
+
+	const score = {
+		score : mostRecentScore,
+		name: username.value
+	};
+	highScores.push(score);
+
+	highScores.sort((a,b) =>{
+		return b.score - a.score;
+	});
+	highScores.splice(5);
+
+	localStorage.setItem('highScores', JSON.stringify(highScores));
+	window.location.assign("/templates/");
+	console.log(highScores)
 }
