36ad17742a486d4554ece3b41a139773208530ae
queenfiona <fionaitya@gmail.com>
2019-04-23 10:46:28 +0300

    fetch api from opentdb

--- js/game.js
+++ js/game.js
@@ -3,6 +3,8 @@ const choices = Array.from(document.getElementsByClassName('choice-text'));
 const progressText = document.getElementById('progressText');
 const progressBarFull = document.getElementById('progressBarFull');
 const scoreText = document.getElementById('score');
+const loader = document.getElementById('loader');
+const game = document.getElementById('game');
 
 let currentQuestion = {};
 let acceptingAnswers = false;
@@ -46,6 +48,8 @@ startGame = ()=>{
 	score = 0;
 	availableQuestions = [...questions];
 	getNewQuestion();
+	game.classList.remove("hidden");
+	loader.classList.add("hidden");
 };
 
 getNewQuestion = ()=>{
